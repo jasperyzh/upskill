a4d77b7a9f443d806cc8002dbf21386c3befadd8
jasperyzh <jasper.yzh@gmail.com>
2022-12-19 21:57:38 +0800

    save state for going into phase2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-import purgecss from "astro-purgecss";
+// import purgecss from "astro-purgecss";
 
+console.log(process.env.NODE_ENV);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), purgecss()],
+  integrations: [vue()],
   // npm_run_build
   // base: "/astro"
 
-  // base: "/upskill",
-  
-  // base: "/",
+  base: process.env.NODE_ENV === "production" ? "/upskill" : "",
+
   // production with relative link?
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "./src/styles/component-mixin";`
-        }
-      }
-    }
-  }
+          additionalData: `@import "./src/styles/component-mixin";`,
+        },
+      },
+    },
+  },
 });
